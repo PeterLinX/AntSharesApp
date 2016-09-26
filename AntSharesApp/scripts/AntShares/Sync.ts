68a20dbfe67adca82678249f77c0e1fe5eb2044a
陈志同 <chenzhitong12@hotmail.com>
2016-09-26 13:36:52 +0800

    We add icons in android and ios. We fixed an issue where local height do not display in the status bar when blockchain synchronize.

--- AntSharesApp/scripts/AntShares/Sync.ts
+++ AntSharesApp/scripts/AntShares/Sync.ts
@@ -6,10 +6,18 @@
         {
             Core.Blockchain.Default.getBlockCount().then(result =>
             {
-                let height = result - 1;
-                $("#remote_height").text(height);
+                let remoteHeight = result - 1;
+                $("#remote_height").text(remoteHeight);
+                if (Global.Wallet)
+                {
+                    let localHeight = (Global.Wallet as any).walletHeight;
+                    console.log(localHeight);
+                    $("#local_height").text(localHeight);
+                }
                 setTimeout(Sync.getblockcount, 5000);
             }, reason => setTimeout(Sync.getblockcount, 5000));
         }
     }
+
+    AntShares.Sync.getblockcount();
 }
