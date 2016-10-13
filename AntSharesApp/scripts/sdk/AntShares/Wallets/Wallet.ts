62528e08891bbba619cb4e83bd1138e6f1234ce3
Erik Zhang <erik@vcage.com>
2016-10-13 21:12:22 +0800

    update sdk

--- AntSharesApp/scripts/sdk/AntShares/Wallets/Wallet.ts
+++ AntSharesApp/scripts/sdk/AntShares/Wallets/Wallet.ts
@@ -55,12 +55,12 @@
                 passwordKey = new Uint8Array(result);
                 return Promise.all<any>([
                     window.crypto.subtle.digest("SHA-256", passwordKey),
-                    window.crypto.subtle.importKey("jwk", <any>{ kty: "oct", k: (passwordKey).base64UrlEncode(), alg: "A256CBC", ext: true }, "AES-CBC", false, ["encrypt"])
+                    window.crypto.subtle.importKey("raw", <any>result, "AES-CBC", false, ["encrypt"])
                 ]);
-            }).then(result =>
+            }).then(results =>
             {
-                passwordKeyHash = result[0];
-                return window.crypto.subtle.encrypt({ name: "AES-CBC", iv: this.iv }, result[1], this.masterKey);
+                passwordKeyHash = results[0];
+                return window.crypto.subtle.encrypt({ name: "AES-CBC", iv: this.iv }, results[1], this.masterKey);
             }).then(result =>
             {
                 return Promise.all([
@@ -118,7 +118,7 @@
         {
             if (encryptedPrivateKey == null) throw new RangeError();
             if (encryptedPrivateKey.byteLength != 112) throw new RangeError();
-            return window.crypto.subtle.importKey("jwk", <any>{ kty: "oct", k: this.masterKey.base64UrlEncode(), alg: "A256CBC", ext: true }, "AES-CBC", false, ["decrypt"]).then(result =>
+            return window.crypto.subtle.importKey("raw", this.masterKey, "AES-CBC", false, ["decrypt"]).then(result =>
             {
                 return window.crypto.subtle.decrypt({ name: "AES-CBC", iv: this.iv }, result, encryptedPrivateKey);
             }).then(result =>
@@ -154,7 +154,7 @@
 
         protected encryptPrivateKey(decryptedPrivateKey: Uint8Array): PromiseLike<Uint8Array>
         {
-            return window.crypto.subtle.importKey("jwk", <any>{ kty: "oct", k: this.masterKey.base64UrlEncode(), alg: "A256CBC", ext: true }, "AES-CBC", false, ["encrypt"]).then(result =>
+            return window.crypto.subtle.importKey("raw", this.masterKey, "AES-CBC", false, ["encrypt"]).then(result =>
             {
                 return window.crypto.subtle.encrypt({ name: "AES-CBC", iv: this.iv }, result, decryptedPrivateKey);
             }).then(result =>
@@ -341,7 +341,6 @@
 
         protected init(path: string, password: string | ArrayBuffer, create: boolean): PromiseLike<void>
         {
-            console.log(password);
             this.path = path;
             let passwordKey: Uint8Array, passwordKeyHash: Uint8Array, aesKey: any;
             let current_version = new Uint8Array([0, 7, 0, 0]);
@@ -350,7 +349,7 @@
                 passwordKey = new Uint8Array(result);
                 return Promise.all<any>([
                     window.crypto.subtle.digest("SHA-256", passwordKey),
-                    window.crypto.subtle.importKey("jwk", <any>{ kty: "oct", k: passwordKey.base64UrlEncode(), alg: "A256CBC", ext: true }, "AES-CBC", false, ["encrypt", "decrypt"])
+                    window.crypto.subtle.importKey("raw", passwordKey, "AES-CBC", false, ["encrypt", "decrypt"])
                 ]);
             }).then(results =>
             {
