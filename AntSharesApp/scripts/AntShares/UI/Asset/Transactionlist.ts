dfd548e67d1877fc318ff3cf7b1da1a44d8d25ff
PeterLinX <18610565179@163.com>
2016-11-03 16:18:44 +0800

    add feature: transactionlist

--- AntSharesApp/scripts/AntShares/UI/Asset/Transactionlist.ts
+++ AntSharesApp/scripts/AntShares/UI/Asset/Transactionlist.ts
@@ -11,8 +11,8 @@
                 TabBase.showTab("#Tab_Wallet_Open");
                 return;
             }
-            let ul = $("#Tab_Asset_TransactionList").find("ul:eq(0)");
-            ul.find("li :visible").remove();
+            let tbody = $("#Tab_Asset_TransactionList").find("tbody:eq(0)");
+            tbody.find("tr :visible").remove();
             this.db = new AntShares.Implementations.Wallets.IndexedDB.WalletDataContext(Global.Wallet.dbPath);
             this.db.open().then(() => {
                 this.loadTransactionList();
@@ -22,6 +22,7 @@
         private loadTransactionList = (): PromiseLike<any> => {
             let _transaction = this.db.transaction("Transaction", "readonly");
             let arrayTransaction = new Array<any>();
+            let execute: () => PromiseLike<void>;
             _transaction.store("Transaction").openCursor().onsuccess = e => {
                 let cursor = <IDBCursorWithValue>(<IDBRequest>e.target).result;
                 if (cursor) {
@@ -32,71 +33,43 @@
             return _transaction.commit().then(() => {
                 if (arrayTransaction.length <= 0) {
                     $("#Tab_Asset_TransactionList > h5").show();
-                    $("#Tab_Asset_TransactionList > button").hide();
                     return;
                 }
                 else {
                     $("#Tab_Asset_TransactionList > h5").hide();
-                    arrayTransaction.forEach(result => {
-                        let ul = $("#Tab_Asset_TransactionList").find("ul:eq(0)");
-                        let liTemplet = ul.find("li:eq(0)");
-                        let li = liTemplet.clone(true);
-                        li.removeAttr("style");
-                        
+                    let txArray = linq(arrayTransaction).orderByDescending(p => p.time).toArray();
+                    console.log(txArray);
+                    let result = Promise.resolve();
+                    execute = function (): PromiseLike<void> {
+                        for (let i = 0; i < txArray.length; i++) {
+                            let tbody = $("#Tab_Asset_TransactionList").find("tbody:eq(0)");
+                            let trTemp = tbody.find("tr:eq(0)");
+                            let tr = trTemp.clone(true);
+                            tr.removeAttr("style");
 
-                        let tx = Core.Transaction.deserializeFrom(result.rawData.hexToBytes().buffer);
-                        return tx.ensureHash().then(result => {
-                            li.find(".tx-id").text(tx.hash.toString());
-                            tx.inputs.forEach(input =>
-                            {
-                                Global.Blockchain.getTransaction(input.prevHash).then((tx) =>
-                                {
-                                    var inputScriptHash = tx.outputs[input.prevIndex].scriptHash;
-                                    Wallets.Wallet.toAddress(inputScriptHash).then(
-                                        (inputAddress) =>
-                                        {
-                                            var inputDiv = document.createElement("div");
-                                            inputDiv.innerText = inputAddress;
-                                            li.find(".tx-input").append(inputDiv);
-                                        })
-                                    
-                                })
-                            })
-                            tx.outputs.forEach(output =>
-                            {
-                                Wallets.Wallet.toAddress(output.scriptHash).then(
-                                    (address) =>
-                                    {
-                                        var outputDiv = document.createElement("div");
-                                        outputDiv.innerText = address;
-                                        li.find(".tx-output").append(outputDiv);
-                                    })
-                                
-
-                                var valueDiv = document.createElement("div");
-                                valueDiv.innerText = output.value.toString();
-                                li.find(".tx-value").append(valueDiv);
-
-                                Global.Blockchain.getTransaction(output.assetId).then((tx) =>
-                                {
-                                    var asset = tx as Core.RegisterTransaction;
-                                    var assetDiv = document.createElement("div");
-                                    assetDiv.innerText = asset.getName();
-                                    li.find(".tx-asset").append(assetDiv);
+                            let tx = Core.Transaction.deserializeFrom(txArray[i].rawData.hexToBytes().buffer);
+                            result = result.then(() => {
+                                return tx.ensureHash();
+                            }).then(() => {
+                                return Global.Blockchain.getBlockCount();
+                                }).then(currentHeight => {
+                                    let txHeight: number = txArray[i].height;
+                                    tr.find(".tx-time").text(new Date(parseInt(txArray[i].time) * 1000).toLocaleString());
+                                    tr.find(".tx-id").text(tx.hash.toString());
+                                    tr.find(".tx-confirm").text(currentHeight - txHeight);
+                                    tr.find(".tx-type").text(Core.TransactionType[tx.type]);
+                                    tbody.append(tr);
                                 });
-                                
-                            })
-                            ul.append(li);
-                        });
-                    });
+                        }
+                        return result;
+                    }
                 }
-
-
+            }).then(() => {
+                execute();
+            }).catch(e => {
+                alert(e);
             });
         }
-
-
-
 
     }
 }
