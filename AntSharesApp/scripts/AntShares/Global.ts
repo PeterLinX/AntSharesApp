32f8c67c0cc4524921c2cda32cbd99fdb25e6198
chenzhitong <chenzhitong12@qq.com>
2017-04-10 17:50:39 +0800

    Modify the logic automatically selects the node, seed2 or seed8 failure doesn't affect app using.

--- AntSharesApp/scripts/AntShares/Global.ts
+++ AntSharesApp/scripts/AntShares/Global.ts
@@ -9,37 +9,41 @@
 
         static chooseNode()
         {
-            let start = new Date().getTime();
+            let isChoosed = false;
             let seed2 = new Network.RPC.RpcClient("https://seed2.antshares.org:10331/");
             let seed8 = new Network.RPC.RpcClient("https://seed8.antshares.org:10331");
-            $("#seed").text("中国上海 https://seed2.antshares.org:10331");
+
+            let start = new Date().getTime();
             new Implementations.Blockchains.RPC.RpcBlockchain(seed2).getBlockCount().then(result =>
             {
                 let end = new Date().getTime();
                 let timeSpan = end - start;
                 $("#seed2").text(timeSpan);
+                if (!isChoosed) {
+                    isChoosed = true;
+                    Global.Blockchain = Core.Blockchain.registerBlockchain(new Implementations.Blockchains.RPC.RpcBlockchain(seed2));
+                    $("#seed").text("中国上海 https://seed2.antshares.org:10331");
+                }
 
-                let start2 = new Date().getTime();
-                new Implementations.Blockchains.RPC.RpcBlockchain(seed8).getBlockCount().then(result =>
-                {
-                    let end2 = new Date().getTime();
-                    let timeSpan2 = end2 - start2;
-                    $("#seed8").text(timeSpan2);
+            });
 
-                    if (timeSpan2 < timeSpan)
-                    {
-                        Global.Blockchain = Core.Blockchain.registerBlockchain(new Implementations.Blockchains.RPC.RpcBlockchain(seed8));
-                        $("#seed").text("英国伦敦 https://seed8.antshares.org:10331");
-                    }
-                });
-
+            let start2 = new Date().getTime();
+            new Implementations.Blockchains.RPC.RpcBlockchain(seed8).getBlockCount().then(result => {
+                let end2 = new Date().getTime();
+                let timeSpan2 = end2 - start2;
+                $("#seed8").text(timeSpan2);
+                if (!isChoosed) {
+                    isChoosed = true;
+                    Global.Blockchain = Core.Blockchain.registerBlockchain(new Implementations.Blockchains.RPC.RpcBlockchain(seed8));
+                    $("#seed").text("英国伦敦 https://seed8.antshares.org:10331");
+                }
             });
         }
     }
 
     $(function ()
     {
-        //Global.chooseNode();
+        Global.chooseNode();
     });
 
     
