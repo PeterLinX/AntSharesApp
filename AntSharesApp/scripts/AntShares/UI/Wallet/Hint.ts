7d5ed75a125b0dfe52e7506d09c4f6bd2eba60f3
PeterLinX <18610565179@163.com>
2017-08-21 11:25:23 +0800

    update

---
+++ AntSharesApp/scripts/AntShares/UI/Wallet/Hint.ts
@@ -0,0 +1,25 @@
+﻿namespace AntShares.UI.Wallet {
+    export class Hint extends TabBase {
+
+        private db: AntShares.Implementations.Wallets.IndexedDB.WalletDataContext;
+
+        protected oncreate(): void {
+            $(this.target).find("#export_prikey").click(this.showPrivateKey);
+
+        }
+
+        protected onload(args: any[]): void {
+            //if (Global.Wallet == null) {
+            //    TabBase.showTab("#Tab_Wallet_Open");
+            //    return;
+            //}
+        }
+
+        private showPrivateKey() {
+            Global.Wallet.getAccounts()[0].export().then(result => {
+                alert(result);
+            });
+        }
+        
+    }
+}
